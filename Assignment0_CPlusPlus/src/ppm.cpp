13d85e25824d9b3d3fb6aab54b5842e11cb47ff6
jzhang1223 <zhang.just@husky.neu.edu>
2020-01-22 22:28:56 -0500

    Read some header values and save to file properly

--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -14,8 +14,10 @@ PPM::PPM(std::string fileName){
 
     inFile.open(fileName);
 
-    unsigned char maxValue;
+    // unsigned char maxValue;
     int pixelsFilled = 0;
+
+    std::cout << "Loading PPM Named: " << fileName << "\n";
 
     if(inFile.is_open()) {
         std::string line;
@@ -23,22 +25,39 @@ PPM::PPM(std::string fileName){
         while(getline(inFile,line)) {
             // do something to the line
             if (skipFirst || line[0] == '#') {
+                std::cout << "SKIPPING: " << line << "\n";
                 skipFirst = false;
                 continue;
             }
 
+            std::cout << line << "\n";
+
             if (!m_width) {
-                m_width = line[0];
-                m_height = line[2];
-                m_PixelData = new unsigned char[m_width * m_height * 3];
-            } else if (!maxValue) {
-                maxValue = line[0];
-            } else {
-                m_PixelData[pixelsFilled] = line[0];
-                pixelsFilled += 1;
+                std::cout << "STORING m_width AS: " << line[0] << "\n";
+                // if (line[0] == "1") {
+                //     std::cout << "STRING-STRING-STRING\n";
+                // }
+                // if (line[0] == '1') {
+                //     std::cout << "CHAR-CHAR-CHAR\n";
+                // }
+                m_width = line[0] - '0';
+                std::cout << "STORING m_height AS: " << line[2] << "\n";
+                m_height = line[2] - '0';
             }
+            //     m_PixelData = new unsigned char[m_width * m_height * 3];
+            // } else if (!maxValue) {
+            //     maxValue = line[0];
+            // } else {
+            //     m_PixelData[pixelsFilled] = line[0];
+            //     pixelsFilled += 1;
+            // }
         }
     }
+
+    std::cout << "Finished Loading PPM\n";
+
+    inFile.close();
+
 }
 
 // Destructor clears any memory that has been allocated
@@ -51,20 +70,26 @@ void PPM::savePPM(std::string outputFileName){
     std::ofstream outFile;
     outFile.open(outputFileName);
 
-    outFile << "P3" << std::endl;
-    outFile << "# Comment" << std::endl;
-    outFile << m_width << " " << m_height << std::endl;
+    std::cout << "Saving PPM named: " << outputFileName << "\n";
+
+    outFile << "P3\n";
+    outFile << "# Comment\n";
+
+    std::cout << "WRITING: " << m_width << " " << m_height << "\n";
+    outFile << m_width << " " << m_height << "\n";
     // TODO replace this 
-    outFile << "MAX VALUE" << std::endl;
+    outFile << "MAX VALUE\n";
 
     // for(int i = 0; this->m_PixelData[i] != '\0'; i++) {
 
     // for(int i = 0; i < sizeof(m_PixelData) / sizeof(m_PixelData[0]); i++) {
-    
+
     // for(int i = 0; i < 6; i++) {
     //     std::cout << i;
     //     outFile << this->m_PixelData[i] << "\n";
     // }
+
+    std::cout << "Finished Saving PPM\n";
 
     outFile.close();
 
