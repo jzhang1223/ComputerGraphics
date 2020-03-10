c7d52323c3ae601d1979a67d6d8c70b7cea3f3f3
jzhang1223 <zhang.just@husky.neu.edu>
2020-03-10 15:35:05 -0400

    Match cat image to ppm

--- Lab7_AbstractionsAndTextures/BasicWidget.cpp
+++ Lab7_AbstractionsAndTextures/BasicWidget.cpp
@@ -55,15 +55,10 @@ void BasicWidget::initializeGL()
   // TODO:  Make sure to add texture coordinates to pass into the initialization of our renderable
   idx << 0 << 1 << 2 << 2 << 1 << 3;
 
-  texCoord << QVector2D(0.0, 0.0);
+  texCoord << QVector2D(1.0, 1.0);
   texCoord << QVector2D(1.0, 0.0);
   texCoord << QVector2D(0.0, 1.0);
-  texCoord << QVector2D(1.0, 1.0);
-
-  // model_.setToIdentity();
-  // view_.setToIdentity();
-  // projection_.setToIdentity();
-
+  texCoord << QVector2D(0.0, 0.0);
 
   Renderable* ren = new Renderable();
   ren->init(pos, norm, texCoord, idx, texFile);
