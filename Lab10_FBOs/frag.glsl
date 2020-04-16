37e5d67bb31c38a506c6df2805b5b526bc3707d3
jzhang1223 <zhang.just@husky.neu.edu>
2020-04-15 20:28:06 -0400

    Add edge-detection effect

--- Lab10_FBOs/frag.glsl
+++ Lab10_FBOs/frag.glsl
@@ -34,5 +34,37 @@ uniform PointLight pointLights[1];  // Our lights
 void main() {
   // Set our output fragment color to whatever we pull from our input texture (Note, change 'tex' to whatever the sampler is named)
   //fragColor = texture(colorTex, texCoords);
-  fragColor = vec4(vec3(1.0 - texture(colorTex, texCoords)), 1.0);
+  //fragColor = vec4(vec3(1.0 - texture(colorTex, texCoords)), 1.0);
+
+  float offset = 1.0/30.0;
+
+  vec2 offsets[9] = vec2[](
+        vec2(-offset, offset),
+        vec2(0.0f, offset),
+        vec2(offset, offset),
+        vec2(-offset, 0.0f),
+        vec2(0.0f, 0.0f),
+        vec2(offset, 0.0f),
+        vec2(-offset, -offset),
+        vec2(0.0f, -offset),
+        vec2(offset, -offset)
+        );
+
+  float kernel[9] = float[](1, 1, 1,
+                            1, -8, 1,
+                            1, 1, 1);
+
+  
+  vec3 sampleTex[9];
+  for(int i = 0; i < 9; i++) {
+    sampleTex[i] = vec3(texture(colorTex, texCoords.st + offsets[i]));
+  }
+
+  vec3 col = vec3(0.0);
+  for(int i = 0; i < 9; i++) {
+    col += sampleTex[i] * kernel[i];
+  }
+
+  fragColor = vec4(col, 1.0);
+
 }
